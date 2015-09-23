7da6fe7951430d036b7657833f12d96c99716aa8
Chris Lytsell & Rick Hills <student@epicodus.com>
2015-09-23 15:28:05 -0700

    add a default category option

--- controllers/QuestionsController.js
+++ controllers/QuestionsController.js
@@ -2,4 +2,7 @@ questionList.controller('QuestionsCtrl', function QuestionsCtrl($scope, Question
 	$scope.questions = QuestionsFactory.questions;
 	$scope.categories = QuestionsFactory.categories;
 	$scope.QuestionsFactory = QuestionsFactory;
+	$scope.questionCategory = QuestionsFactory.categories[0];
+
+	var defaultCategory = QuestionsFactory.categories[0];
 });
