a0dea4b0341e53211920f8061ea0013e41b26c30
Rick Hills <rh-jobs@outlook.com>
2015-09-25 11:04:36 -0700

    Add buttons for sign in and out, 2-way binding for them, and function to toggle boolean values.

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -3,4 +3,9 @@ attendanceTracker.controller('StudentsCtrl', function StudentsCtrl($scope, Stude
 	$scope.students = StudentsFactory.students; 
 	$scope.StudentsFactory = StudentsFactory; 
 
+	$scope.updateLoginStatus = function(student) {
+		// toggles status between true and false
+		student.loggedIn = student.loggedIn ? false:true; 
+	}
+
 }); 
